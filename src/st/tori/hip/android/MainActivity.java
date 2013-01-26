27671303170b4866fb81ced17f9384a31989b0f0
hisashin <shingohisakawa@gmail.com>
2013-01-26 10:35:10 +0900

    add Listen&Exec

--- src/st/tori/hip/android/MainActivity.java
+++ src/st/tori/hip/android/MainActivity.java
@@ -1,22 +1,40 @@
 package st.tori.hip.android;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity
-{
+public class MainActivity extends Activity {
+
+	private Button buttonListen;
+	private Button buttonExec;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState)
-	{
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		Button buttonListen = (Button) findViewById(R.id.buttonListen);
+		buttonListen.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getApplicationContext(), ListenActivity.class));
+			}
+		});
+		Button buttonExec = (Button) findViewById(R.id.buttonExec);
+		buttonExec.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				startActivity(new Intent(getApplicationContext(), ExecActivity.class));
+			}
+		});
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu)
-	{
+	public boolean onCreateOptionsMenu(Menu menu) {
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
