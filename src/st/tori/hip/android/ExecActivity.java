591bb1195e2c0d89e4cc38f22c75a7803e537bb9
hisashin <shingohisakawa@gmail.com>
2013-01-26 10:36:15 +0900

    add Listen&Exec

---
+++ src/st/tori/hip/android/ExecActivity.java
@@ -0,0 +1,20 @@
+package st.tori.hip.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+
+public class ExecActivity extends Activity {
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_exec);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_exec, menu);
+		return true;
+	}
+}
